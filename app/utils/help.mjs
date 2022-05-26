8b11b7352e32b9059645cd34e696be12d4715ba9
Desenvolvedor-Dharma <hpedrobs.dev@gmail.com>
2022-05-26 16:06:30 -0300

    Create help.mjs

---
+++ app/utils/help.mjs
@@ -0,0 +1,13 @@
+export default () => {
+    console.log('\nUse:\n')
+    console.log('--insertXmlPath="pathname"       Enter the path name of the xmls folder')
+    console.log('--listXmlPaths                   Return to xmls folder path name list')
+    console.log('--removeXmlPaths                 Remove all paths from the xmls folder')
+    console.log('--processLoader                  Add XML to Queue')
+    console.log('--company="string"               Filter by company folder')
+    console.log('--yearStart=number               year to start')
+    console.log('--monthStart=number              month to start')
+    console.log('--yearEnd=number                 year to finish')
+    console.log('--monthEnd=number                month to finish')
+    console.log('--months=number                  Set previous month')
+}
