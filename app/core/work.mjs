3142dc77e3e5e45a2d42be8bc7e1501f2c0f0884
Desenvolvedor-Dharma <hpedrobs.dev@gmail.com>
2022-05-26 16:05:08 -0300

    Create work.mjs

---
+++ app/core/work.mjs
@@ -0,0 +1,13 @@
+/* Internal */
+import path from 'path'
+import outstandingNfes from '../schemas/outstanding-nfes.js'
+import clg from './clg.mjs'
+
+/* Core Module */
+import fs from 'fs'
+
+export default class Work {
+    async exec () {
+
+    }
+}
