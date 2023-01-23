13e90516d04126ffe664845e95afa81c693be235
PH <88108152+hpedrobs@users.noreply.github.com>
2023-01-23 13:10:32 -0300

    Delete clearOutstandingNfes.mjs

--- app/services/clearOutstandingNfes.mjs
+++
@@ -1,9 +0,0 @@
-import model from '../schemas/outstanding-nfes.js'
-
-export default async function () {
-    try {
-        await model.dropTable()
-    } catch (err) {
-        console.log(err)
-    }
-}
