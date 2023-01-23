2582b88d7117e8a366c6c95dd5a5982807757d7a
PH <88108152+hpedrobs@users.noreply.github.com>
2023-01-23 13:10:36 -0300

    Delete insertXmlPath.mjs

--- app/services/insertXmlPath.mjs
+++
@@ -1,9 +0,0 @@
-import model from '../schemas/xmlpaths.js'
-
-export default async function (pathname) {
-    try {
-        await model.createOne({ pathname })
-    } catch (err) {
-        console.log(err)
-    }
-}
